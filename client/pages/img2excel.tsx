386c5280dc243f5544a8fe25e1cf3bef440c76b8
osawa-koki <github@osawa.dev>
2023-03-21 14:09:38 +0900

    ローディング入れた。

--- client/pages/img2excel.tsx
+++ client/pages/img2excel.tsx
@@ -66,6 +66,19 @@ export default function Img2ExcelPage() {
             {error}
           </Alert>
         }
+        {
+          loading &&
+          <div className="mt-5 d-flex justify-content-between">
+            <Spinner animation="grow" variant="primary" />
+            <Spinner animation="grow" variant="secondary" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="light" />
+            <Spinner animation="grow" variant="dark" />
+          </div>
+        }
       </div>
     </Layout>
   );
