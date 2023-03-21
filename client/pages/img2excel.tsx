dcaacae5d783100fcd353a2d9bf4d9d01fbe3fc3
osawa-koki <github@osawa.dev>
2023-03-21 13:59:56 +0900

    小さな修正。

--- client/pages/img2excel.tsx
+++ client/pages/img2excel.tsx
@@ -4,10 +4,56 @@ import { Button, Alert, Form, Spinner, Table } from 'react-bootstrap';
 import Layout from "../components/Layout";
 import setting from "../setting";
 
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
 export default function Img2ExcelPage() {
+
+  const _canvas = React.createRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    canvas = _canvas.current as HTMLCanvasElement;
+    ctx = canvas.getContext("2d");
+  }, [_canvas]);
+
+  const FileRegister = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const img = new Image();
+        img.src = reader.result as string;
+        img.onload = () => {
+          const width = img.width;
+          const height = img.height;
+
+          canvas.width = width;
+          canvas.height = height;
+
+          if (width * height < 128 * 128) {
+            ctx.drawImage(img, 0, 0);
+          } else {
+            console.log("Image size is not less than 128x128.");
+          }
+        };
+      }
+      reader.readAsDataURL(e.currentTarget.files[0]);
+    }
+  };
+
+  const Img2Excel = async () => {
+    const base64 = canvas.toDataURL("image/png");
+    console.log("Base64-encoded image: ", base64);
+  };
+
   return (
     <Layout>
       <div id="Img2Excel">
+        <Form.Group className="mt-3">
+          <Form.Label>Select Image File.</Form.Label>
+          <Form.Control type="file" accept="image/*" onInput={FileRegister} />
+        </Form.Group>
+        <canvas ref={_canvas} style={{width: "300px", height: "300px"}} className="mt-3 d-block m-auto border"></canvas>
+        <Button variant="primary" className="mt-3 d-block m-auto" onClick={() => {Img2Excel()}}>Convert</Button>
       </div>
     </Layout>
   );
