9b956776a251430bb90626120f22ba879719e33a
osawa-koki <github@osawa.dev>
2023-03-21 14:29:14 +0900

    テストコード消した。

--- server/test/controllers/imge2_excel_controller_test.rb
+++ server/test/controllers/imge2_excel_controller_test.rb
@@ -2,7 +2,5 @@ require "test_helper"
 
 class Imge2ExcelControllerTest < ActionDispatch::IntegrationTest
   test "should get post" do
-    get imge2_excel_post_url
-    assert_response :success
   end
 end
